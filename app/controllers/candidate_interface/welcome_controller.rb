c1d9fd0e1de82d7201ad8e6e6fff264322900734
Tijmen Brommet <tijmen@gmail.com>
2019-10-07 16:59:40 +0100

    ***

--- app/controllers/candidate_interface/welcome_controller.rb
+++ app/controllers/candidate_interface/welcome_controller.rb
@@ -3,7 +3,30 @@ module CandidateInterface
     before_action :authenticate_candidate!
 
     def show
+      @contact_details_form = ContactDetailsForm.new(application_form)
+
       redirect_to candidate_interface_welcome_path if params[:token]
+    end
+
+    def update
+      form = ContactDetailsForm.new(params[:x])
+      form.save
+    end
+
+    class ContactDetailsForm
+      include ActiveModel::Model
+      attr_access :first_name
+      validates_present_of :first_name
+
+      def load(application_form)
+        # buid from self
+      end
+
+      def save
+        application_form.update!(
+
+        )
+      end
     end
   end
 end
