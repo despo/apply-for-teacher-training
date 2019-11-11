aa9c1a412bd3dd12cd85eeaa6ca4f06fe0605149
Tijmen Brommet <tijmen@gmail.com>
2019-11-11 13:57:06 +0000

    Remove Basic auth for provider interface
    We're replacing this with DfE Signin.

--- app/controllers/provider_interface/provider_interface_controller.rb
+++ app/controllers/provider_interface/provider_interface_controller.rb
@@ -1,7 +1,5 @@
 module ProviderInterface
   class ProviderInterfaceController < ActionController::Base
-    include BasicAuthHelper
-    before_action :require_basic_auth_for_ui
     layout 'application'
   end
 end
