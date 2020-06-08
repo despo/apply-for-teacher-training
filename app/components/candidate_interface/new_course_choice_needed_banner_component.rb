82f2c293289d8cb2d65cf4971b5435807a367b3e
David Gisbey <davidgisbey@gmail.com>
2020-06-08 15:58:59 +0100

    Rework the component to use the method on the application form

--- app/components/candidate_interface/new_course_choice_needed_banner_component.rb
+++ app/components/candidate_interface/new_course_choice_needed_banner_component.rb
@@ -9,19 +9,7 @@ module CandidateInterface
     end
 
     def render?
-      a_course_has_been_withdrawn? || an_application_choice_has_become_full?
+      @application_form.course_choices_that_need_replacing.any?
     end
-
-  private
-
-    def a_course_has_been_withdrawn?
-      application_form.application_choices.map(&:course_option).select(&:course_withdrawn?).any?
-    end
-
-    def an_application_choice_has_become_full?
-      application_form.application_choices.map(&:course_option).select(&:no_vacancies?).any?
-    end
-
-    attr_reader :application_form
   end
 end
