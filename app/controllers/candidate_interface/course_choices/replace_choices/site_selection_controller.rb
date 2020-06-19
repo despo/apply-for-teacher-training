4b9f7d609dce3d9a4fd3989fcbde3b79f011c05f
David Gisbey <davidgisbey@gmail.com>
2020-06-19 10:44:43 +0100

    Add logic to check for duplicate coursee and to pass params onto confim

--- app/controllers/candidate_interface/course_choices/replace_choices/site_selection_controller.rb
+++ app/controllers/candidate_interface/course_choices/replace_choices/site_selection_controller.rb
@@ -3,11 +3,15 @@ module CandidateInterface
     module ReplaceChoices
       class SiteSelectionController < BaseController
         def new
-          @pick_site = PickSiteForm.new(
-            provider_id: params.fetch(:provider_id),
-            course_id: params.fetch(:course_id),
-            study_mode: params.fetch(:study_mode),
-          )
+          if candidate_has_already_chosen_this_course
+            redirect_to candidate_interface_replace_course_choices_course_path(params['id'], params['provider_id'])
+          else
+            @pick_site = PickSiteForm.new(
+              provider_id: params.fetch(:provider_id),
+              course_id: params.fetch(:course_id),
+              study_mode: params.fetch(:study_mode),
+            )
+          end  
         end
 
         def update
@@ -21,11 +25,38 @@ module CandidateInterface
           @replacement_course_option_id = params.dig('candidate_interface_pick_site_form', 'course_option_id')
           @pick_site = create_pick_site_form(@course_choice, @replacement_course_option_id)
 
-          if @pick_site.valid?
+          if params['provider_id'].present? && @pick_site.valid?
+            redirect_to candidate_interface_confirm_replacement_course_choice_path(
+              @course_choice.id,
+              @replacement_course_option_id,
+              provider_id: params['provider_id'],
+              course_id: params['course_id'],
+              study_mode: params['study_mode'],
+            )
+          elsif @pick_site.valid?
             redirect_to candidate_interface_confirm_replacement_course_choice_path(@course_choice.id, @replacement_course_option_id)
           else
             flash[:warning] = 'Please select a new location.'
             redirect_to candidate_interface_replace_course_choice_update_location_path(@course_choice.id)
+          end
+        end
+
+      private
+
+        def candidate_has_already_chosen_this_course
+          provider = Provider.find(params.fetch(:provider_id))
+          course = provider.courses.find(params.fetch(:course_id))
+
+          course_already_chosen = current_application
+            .application_choices
+            .includes([:course])
+            .any? { |application_choice| application_choice.course == course }
+
+          if course_already_chosen
+            flash[:warning] = I18n.t!('errors.application_choices.already_added', course_name_and_code: course.name_and_code)
+            true
+          else
+            false
           end
         end
       end
