73f8943e644ad622046ec0e10eb642d902219c36
Tijmen Brommet <tijmen@gmail.com>
2020-10-27 11:12:08 +0000

    Revert "Migration to remove the references completed column"

--- db/migrate/20201022125243_remove_references_completed_from_application_form.rb
+++
@@ -1,9 +0,0 @@
-class RemoveReferencesCompletedFromApplicationForm < ActiveRecord::Migration[6.0]
-  def up
-    remove_column :application_forms, :references_completed
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
