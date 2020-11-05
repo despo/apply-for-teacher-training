b6d2752c37bd63b909853f2406fef20eebd243c2
David Gisbey <davidgisbey@gmail.com>
2020-11-05 21:40:22 +0000

    Add the ApplicationFeedback table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_151036) do
+ActiveRecord::Schema.define(version: 2020_11_05_145325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -63,6 +63,22 @@ ActiveRecord::Schema.define(version: 2020_11_04_151036) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "working_pattern"
     t.index ["application_form_id"], name: "index_application_experiences_on_application_form_id"
+  end
+
+  create_table "application_feedback", force: :cascade do |t|
+    t.string "section", null: false
+    t.string "path", null: false
+    t.string "page_title", null: false
+    t.boolean "issues", default: false, null: false
+    t.boolean "understands_section", default: true, null: false
+    t.boolean "need_more_information", default: false, null: false
+    t.boolean "answer_does_not_fit_format", default: false, null: false
+    t.string "other_feedback"
+    t.boolean "consent_to_be_contacted", default: false, null: false
+    t.bigint "application_form_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["application_form_id"], name: "index_application_feedback_on_application_form_id"
   end
 
   create_table "application_forms", force: :cascade do |t|
@@ -547,6 +563,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_151036) do
   add_foreign_key "application_choices", "course_options"
   add_foreign_key "application_choices", "course_options", column: "offered_course_option_id"
   add_foreign_key "application_experiences", "application_forms", on_delete: :cascade
+  add_foreign_key "application_feedback", "application_forms", on_delete: :cascade
   add_foreign_key "application_forms", "application_forms", column: "previous_application_form_id"
   add_foreign_key "application_forms", "candidates", on_delete: :cascade
   add_foreign_key "application_qualifications", "application_forms", on_delete: :cascade
